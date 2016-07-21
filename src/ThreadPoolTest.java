247229d76ce02e2358956b5d97942b923129624a
tkzn2 <sakitarou_toea@live.jp>
2016-07-21 18:18:28 +0900

    commit

--- src/ThreadPoolTest.java
+++ src/ThreadPoolTest.java
@@ -6,10 +6,7 @@ public class ThreadPoolTest {
 
 	public final static int THREAD_NUM = 10;
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-		int[] ar = {9,4,2,7,5,3,6,8,1};
-		int[]  ar2 = {1,1,1,0,0,0,1,1,1};
+
 		Vector<Integer> list = new Vector<>();
 		Vector<Integer> list2;
 		Random rnd = new Random();
@@ -18,42 +15,38 @@ public class ThreadPoolTest {
 		QuickSortThread<Integer> qst;
 		QuickSort<Integer> qs;
 
-		for(int j = 0;j < 1;j++)
+		for(int i = 0;i < 100;i++)
 		{
-			
-			for(int i = 0;i < 100;i++)
-			{
-				list.add(rnd.nextInt(100));
-			}
-			
-			list2 = new Vector<>(list);
-			
-			qst = new QuickSortThread<>(list);
-			qs = new QuickSort<>(list2);
-
-			ForkJoinPool fjp = new ForkJoinPool();
-			
-			start = System.nanoTime();
-			
-			fjp.invoke(qst);
-			
-			end = System.nanoTime();
-			System.out.println("Time:" + (end - start) / 1000000f + "ms");
-			
-			fjp.shutdown();
-			
-			qst.print();
-			
-			start = System.nanoTime();
-			
-			qs.sort();
-			
-			end = System.nanoTime();
-
-			System.out.println("Time:" + (end - start) / 1000000f + "ms");
-			
-			qs.print();
+			list.add(rnd.nextInt(100));
 		}
+
+		list2 = new Vector<>(list);
+
+		qst = new QuickSortThread<>(list);
+		qs = new QuickSort<>(list2);
+
+		ForkJoinPool fjp = new ForkJoinPool();
+
+		start = System.nanoTime();
+
+		fjp.invoke(qst);
+
+		end = System.nanoTime();
+		System.out.println("Time:" + (end - start) / 1000000f + "ms");
+
+		fjp.shutdown();
+
+		qst.print();
+
+		start = System.nanoTime();
+
+		qs.sort();
+
+		end = System.nanoTime();
+
+		System.out.println("Time:" + (end - start) / 1000000f + "ms");
+
+		qs.print();
 	}
 }
 
